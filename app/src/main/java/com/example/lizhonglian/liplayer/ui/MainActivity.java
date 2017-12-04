325cd041754eb7640c6fc80741fc526909523a05
lizhonglian <zhonglianli2009@163.com>
2017-12-04 20:25:49 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lizhonglian/liplayer/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.lizhonglian.liplayer.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lizhonglian.liplayer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
